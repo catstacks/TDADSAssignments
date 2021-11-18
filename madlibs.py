260c05c19b0d05eb0e19de6c8195086c0ec897eb
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-18 11:40:33 +0000

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#madlibs template source: http://unplugtoconnect.org/2020/04/27/unplugged-stories-make-your-own-mad-libs/
+
+animal = ""
+country = ""
+plural_noun = ""
+food = ""
+screen_device = ""
+noun = ""
+verb = ""
+adjective = ""
+whitespace = " "
+story_text = "The majestic" + whitespace + animal + whitespace + "has roamed the forests of" + whitespace + country + whitespace + "for thousands of years. Today, she wanders in search of" + whitespace + plural_noun + whitespace + ". She must find food to survive. While hunting for" + whitespace + food + whitespace + ", she found a/an" + whitespace + screen_device + whitespace + "hidden behind a" + whitespace + noun + whitespace + ". She has never seen anything like this before. What will she do? With the device in her teeth, she tries to" + whitespace + verb + whitespace + ", but nothing happens. She takes it back to her family. When her family sees it, they quickly" + whitespace + verb + whitespace + ". Soon, the device becomes" + whitespace + adjective + whitespace + ", and the family decides to put it back where they found it."

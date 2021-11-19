9295ec5a7de5222652a95e062de3c6602c69a812
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-19 14:47:56 +0000

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,14 +1,9 @@
 import random
 
-# This project uses the random module in Python. 
-# The program will first randomly generate a number unknown to the user. 
-# The user needs to guess what that number is. (In other words, the user needs to be able to input information.) 
-# If the user’s guess is wrong, the program should return some sort of indication as to how wrong (e.g. The number is too high or too low). 
-# If the user guesses correctly, a positive indication should appear. 
-# You’ll need functions to check if the user input is an actual number, to see the difference between the inputted number and the randomly generated numbers, and to then compare the numbers. Random function, Variables, Integers, Input/Output, Print, While loops, If/Else statements
+number = random.randint(1, 100) # this must be outside function so that the randomly generated number is the same if someone tries again. Credit: Folarin Olagbuyiro
 
 def guess_game():
-    number = random.randint(1, 100)
+    
     guess = int(input('Guess a number between 1 and 100. Write your guess here: ')) 
     # Prevent invalid user inputs
     # if guess == int or 100 > guess > 1:  
